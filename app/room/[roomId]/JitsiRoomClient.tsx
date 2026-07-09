793514b160cc47ac955aeddabb99e961d8424ce8
careerous <dev@careerous.local>
2026-07-09 10:45:57 +0700

    fix: switch jitsi domain to guifi to bypass iframe block and auth

--- app/room/[roomId]/JitsiRoomClient.tsx
+++ app/room/[roomId]/JitsiRoomClient.tsx
@@ -61,7 +61,7 @@ export default function JitsiRoomClient({
       </div>
       
       <JitsiMeeting
-        domain="meet.ffmuc.net"
+        domain="meet.guifi.net"
         roomName={`careerous-room-${roomId}`}
         configOverwrite={{
           startWithAudioMuted: true,
